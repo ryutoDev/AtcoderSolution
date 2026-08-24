186483f1543340d9d33302f1d69a5853da054130
ryutoDev <zero.puyo1012@gmail.com>
2026-08-25 06:30:48 +0900

    initial commit

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+
+int main() {
+    dsu uf(5);
+    uf.merge(0, 1);
+
+    cout << uf.same(0, 1) << endl;
+}
